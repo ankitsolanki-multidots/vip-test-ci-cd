da97146cd163803eb3adfcc470503f694038554d
Ankit Solanki <ankit.solanki@multidots.com>
2026-07-14 17:55:21 +0000

    Build changes from 91846dd6fec44df427056692074740ac8b5be6a9
    https://github.com/ankitsolanki-multidots/vip-test-ci-cd/actions/runs/29355656221

---
+++ themes/test-ci-cd/assets/build/blocks/name-search/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bd07199d2921d5ffce38');
